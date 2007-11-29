7c3e53bd64f9cbc6660587d2bda13546178e7350
randolf <randolf>
2007-11-29 21:30:31 +0000

    - Completely rebuilt as the previous version was not working at all

--- SQLTools/MetaDict/Loader_Snapshot.cpp
+++ SQLTools/MetaDict/Loader_Snapshot.cpp
@@ -17,12 +17,15 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
 */
 
+// 08.10.2006 B#XXXXXXX - (ak) handling NULLs for PCTUSED, FREELISTS, and FREELIST GROUPS storage parameters
+
 #include "stdafx.h"
 //#pragma warning ( disable : 4786 )
 #include "MetaDict\Loader.h"
 #include "MetaDict\MetaObjects.h"
 #include "MetaDict\MetaDictionary.h"
 #include "COMMON\StrHelpers.h"
+#include "OCI8/BCursor.h"
 
 #ifdef _DEBUG
 #undef THIS_FILE
@@ -33,32 +36,37 @@ static char THIS_FILE[]=__FILE__;
 
 namespace OraMetaDict 
 {
-    using namespace Oci7;
+    const int cn_snp_owner           = 0;
+    const int cn_snp_table_name      = 1;
+    const int cn_snp_type            = 2;
+    const int cn_snp_start_with      = 3;
+    const int cn_snp_next_time       = 4;
+    const int cn_snp_query           = 5;
+    const int cn_snp_tablespace      = 6;
+    const int cn_snp_pct_free        = 7;
+    const int cn_snp_pct_used        = 8;
+    const int cn_snp_ini_trans       = 9;
+    const int cn_snp_max_trans       = 10;
+    const int cn_snp_initial_extent  = 11;
+    const int cn_snp_next_extent     = 12;
+    const int cn_snp_min_extents     = 13;
+    const int cn_snp_max_extents     = 14;
+    const int cn_snp_pct_increase    = 15;
+    const int cn_snp_freelists       = 16;
+    const int cn_snp_freelist_groups = 17;
+    const int cn_snp_cluster_name    = 18;
 
-#define LPSCSTR static const char*
-
-    const int cn_tbl_owner           = 0;
-    const int cn_tbl_table_name      = 1;
-    const int cn_tbl_tablespace      = 2;
-    const int cn_tbl_pct_free        = 3;
-    const int cn_tbl_pct_used        = 4;
-    const int cn_tbl_ini_trans       = 5;
-    const int cn_tbl_max_trans       = 6;
-    const int cn_tbl_initial_extent  = 7;
-    const int cn_tbl_next_extent     = 8;
-    const int cn_tbl_min_extents     = 9;
-    const int cn_tbl_max_extents     = 10;
-    const int cn_tbl_pct_increase    = 11;
-    const int cn_tbl_freelists       = 12;
-    const int cn_tbl_freelist_groups = 13;
-
-    LPSCSTR csz_log_sttm =
+    LPSCSTR csz_snp_sttm =
         "SELECT /*+RULE*/ "
-          "l.log_owner,"
-          "l.master,"
+          "s.owner,"
+          "s.name,"
+          "s.type,"
+          "s.start_with,"
+          "s.next,"
+          "s.query," 
           "t.tablespace_name,"
           "t.pct_free,"
-          "t.pct_used," 
+          "Nvl(t.pct_used, 30) as pct_used," 
           "t.ini_trans," 
           "t.max_trans," 
           "t.initial_extent,"
@@ -66,57 +74,122 @@ namespace OraMetaDict
           "t.min_extents," 
           "t.max_extents," 
           "t.pct_increase, "
-          "t.freelists,"
-          "t.freelist_groups "
-        "FROM <V_SNAPSHOT_LOGS> l, sys.all_tables t "
-        "WHERE l.log_owner = t.owner "
-          "AND l.log_table = t.table_name "
-          "AND l.log_owner = :owner AND l.master <EQUAL> :name";
+          "Nvl(t.freelists, 1),"
+          "Nvl(t.freelist_groups, 1),"
+          "t.cluster_name " 
+        "FROM <V_SNAPSHOTS> s, sys.all_tables t "
+        "WHERE s.owner = t.owner "
+          "AND s.table_name = t.table_name "
+          "AND s.owner = :owner AND s.name <EQUAL> :name";
 
+    const int cn_clu_tab_col_name = 0;
+    const int cn_clu_column_id    = 1;
+
+    LPSCSTR csz_clu_col_sttm =
+        "SELECT /*+RULE*/ "
+            //"<OWNER>,"                                // if (dba) then "c.owner" else "USER"
+            //"c.cluster_name,"
+            //"c.table_name," 
+	        "c.tab_column_name," 
+	        //"c.clu_column_name," 
+	        "t.column_id "
+        "FROM <SRC>_tab_columns t, <SRC>_clu_columns c "// if (dba) then "dba" else "user"
+        "WHERE t.table_name = c.cluster_name "
+            "AND t.column_name = c.clu_column_name "
+            "AND c.cluster_name = :clu_name "
+            "AND c.table_name = :tab_name " 
+            "<OWNER_EXPRS>";                            // if (dba) then "AND t.owner = c.owner "
+                                                        //               "AND c.owner = :owner"
+   static void load_snp_clu_columns  (OciConnect& con, Snapshot& table, bool useDba);
 
 void Loader::LoadSnapshots (const char* owner, const char* name, bool useLike)
 {
     bool useAll = m_strCurSchema.compare(owner) ? true : false;
 
     Common::Substitutor subst;
-    subst.AddPair("<V_SNAPSHOT_LOGS>", 
-                  useAll ? (m_isVer8 ? "sys.all_snapshot_logs" 
-                                     : "dba_snapshot_logs")
-                         : "user_snapshot_logs");
+    subst.AddPair("<V_SNAPSHOTS>", 
+            useAll ? (m_connect.GetVersion() >= OCI8::esvServer80X ? "sys.all_snapshots" 
+                : "dba_snapshots")
+                         : "user_snapshots");
 
     subst.AddPair("<EQUAL>", useLike ? "like" : "=");
-    subst << csz_log_sttm;
+    subst << csz_snp_sttm;
 
-    AutoDefCursor cur(m_connect, 1024, 4 * 1024);
-    cur.Open();
-    cur.Parse(subst.GetResult());
+    OciCursor cur(m_connect, subst.GetResult());
+    
     cur.Bind(":owner", owner);
     cur.Bind(":name",  name);
 
     cur.Execute();
     while (cur.Fetch())
     {
-        Snapshot& snapshot 
-            = m_dictionary.CreateSnapshot(cur[cn_tbl_owner], cur[cn_tbl_table_name]);
+        Snapshot& snp 
+            = m_dictionary.CreateSnapshot(cur.ToString(cn_snp_owner).c_str(), 
+                                          cur.ToString(cn_snp_table_name).c_str());
         
-        cur[cn_tbl_tablespace].GetTextString(snapshot.m_strTablespaceName);
-        snapshot.m_nInitialExtent  = cur[cn_tbl_initial_extent];
-        snapshot.m_nNextExtent     = cur[cn_tbl_next_extent];
-        snapshot.m_nMinExtents     = cur[cn_tbl_min_extents];
-        snapshot.m_nMaxExtents     = cur[cn_tbl_max_extents];
-        snapshot.m_nPctIncrease    = cur[cn_tbl_pct_increase];
-        
-        snapshot.m_nFreeLists      = cur[cn_tbl_freelists];
-        snapshot.m_nFreeListGroups = cur[cn_tbl_freelist_groups];
-        snapshot.m_nPctFree        = cur[cn_tbl_pct_free];
-        snapshot.m_nPctUsed        = cur[cn_tbl_pct_used];
-        snapshot.m_nIniTrans       = cur[cn_tbl_ini_trans];
-        snapshot.m_nMaxTrans       = cur[cn_tbl_max_trans];
+        cur.GetString(cn_snp_tablespace, snp.m_strTablespaceName);
 
-        cur[cn_tbl_owner     ].GetTextString(snapshot.m_strOwner);
-        cur[cn_tbl_table_name].GetTextString(snapshot.m_strName);
+        snp.m_nInitialExtent  = cur.ToInt(cn_snp_initial_extent);
+        snp.m_nNextExtent     = cur.ToInt(cn_snp_next_extent);
+        snp.m_nMinExtents     = cur.ToInt(cn_snp_min_extents);
+        snp.m_nMaxExtents     = cur.ToInt(cn_snp_max_extents);
+        snp.m_nPctIncrease    = cur.ToInt(cn_snp_pct_increase);
+        
+        snp.m_nFreeLists      = cur.ToInt(cn_snp_freelists);
+        snp.m_nFreeListGroups = cur.ToInt(cn_snp_freelist_groups);
+        snp.m_nPctFree        = cur.ToInt(cn_snp_pct_free);
+        snp.m_nPctUsed        = cur.ToInt(cn_snp_pct_used);
+        snp.m_nIniTrans       = cur.ToInt(cn_snp_ini_trans);
+        snp.m_nMaxTrans       = cur.ToInt(cn_snp_max_trans);
+
+        cur.GetString(cn_snp_owner,      snp.m_strOwner);
+        cur.GetString(cn_snp_table_name, snp.m_strName);
+
+        cur.GetString(cn_snp_type, snp.m_strRefreshType);
+        cur.GetString(cn_snp_start_with, snp.m_strStartWith);
+        cur.GetString(cn_snp_next_time, snp.m_strNextTime);
+        cur.GetString(cn_snp_query, snp.m_strQuery);
+        cur.GetString(cn_snp_cluster_name, snp.m_strClusterName);
+        if (!snp.m_strClusterName.empty())
+            load_snp_clu_columns(cur.GetConnect(), snp, useAll);
+        
     }
+
 }
 
+    static
+    void load_snp_clu_columns (OciConnect& con, Snapshot& snp, bool useAll)
+    {
+        try
+        {
+            Common::Substitutor subst;
+
+            subst.AddPair("<SRC>",         useAll  ? "all" : "user");
+            subst.AddPair("<OWNER_EXPRS>", useAll  ? "AND t.owner = c.owner AND c.owner = :owner" : "");
+
+            subst << csz_clu_col_sttm;
+
+            OciCursor cur(con, subst.GetResult(), 20);
+
+            if (useAll) cur.Bind(":owner", snp.m_strOwner.c_str());
+            cur.Bind(":clu_name", snp.m_strClusterName.c_str());
+            cur.Bind(":tab_name", snp.m_strName.c_str());
+
+            cur.Execute();
+            while (cur.Fetch())
+                snp.m_clusterColumns[cur.ToInt(cn_clu_column_id) - 1] = cur.ToString(cn_clu_tab_col_name);
+        }
+        catch (const OciException& x)
+        {
+            if (useAll && x == 942)
+            {
+                _CHECK_AND_THROW_(0, "DBA_TAB_COLUMNS or DBA_CLU_COLUMNS is not accessble! "
+                                   "Can't capture claster table definition. "
+                                   "Reconnect as schema owner and repeat");
+            }
+            else
+                throw;
+        }
+    }
 
 }// END namespace OraMetaDict
